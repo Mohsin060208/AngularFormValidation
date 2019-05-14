1288326647972f84a0663f95ae2efbbdf1d7916f
Mohsin Nazir <mohsin2851@outlook.com>
2019-05-14 13:36:37 +0500

    Added Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { Model } from './model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angularApp';
+export class AppComponent implements OnInit{
+constructor(){}
+ngOnInit(){
+ 
 }
+powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
+
+  model = new Model(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); 
+  }
+}
+
+  
